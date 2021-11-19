c79a5ddabfd449b60d885d1880ffc10d7f673376
Dan Chamorro <i@danchamorro.com>
2021-11-19 16:04:16 -0500

    send email with an attachment

---
+++ send_with_attachement.py
@@ -0,0 +1,22 @@
+import os
+import yagmail
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Put the os.environ in a variable
+env = os.environ
+
+# Get the email address from the .env file and put it in a variable
+sender = env.get("SENDER")
+recipient = env.get("RECIPIENT")
+subject = "This is the subject"
+contents = """
+This is the contents of the email.
+"""
+
+# Send the email using yagmail and the variables above as arguments to the function send with the contents of the email as the argument to the contents variable in the send function call below it.
+yag = yagmail.SMTP(sender, env.get("APP_PASSWORD"))
+yag.send(to=recipient, subject=subject, contents=contents,
+         attachments=["sample_pdf.pdf"])
+print("Email sent!")
