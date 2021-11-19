f14408e60142deaa1b886a0130a129834ab0d188
Dan Chamorro <i@danchamorro.com>
2021-11-19 15:50:04 -0500

    refactored to login to email only once before the forloop

--- multi_email.py
+++ multi_email.py
@@ -13,16 +13,17 @@ list_of_emails = df["email"].to_list()
 # Put the os.environ in a variable
 env = os.environ
 
+# Login to email once
+sender = env.get("SENDER")
+yag = yagmail.SMTP(sender, env.get("APP_PASSWORD"))
+
 for email in list_of_emails:
     # loop through list of emails and send email to each one in list.
-    sender = env.get("SENDER")
     recipient = email
     subject = "This is the subject"
     contents = """
     This is the contents of the email.
     """
-
-    # Send the email using yagmail and the variables above as arguments to the function send with the contents of the email as the argument to the contents variable in the send function call below it.
-    yag = yagmail.SMTP(sender, env.get("APP_PASSWORD"))
+    # Send the email using yagmail
     yag.send(to=recipient, subject=subject, contents=contents)
 print("Email sent!")
